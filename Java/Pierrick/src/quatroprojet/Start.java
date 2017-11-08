c7f3beb9a65e187708a522e77cf5296abc918700
Yberion <yberion.ins@gmail.com>
2017-11-08 13:46:27 +0100

    Séparation du code Java en trois dossiers pour que chacun puisse faire sa version

---
+++ Java/Pierrick/src/quatroprojet/Start.java
@@ -0,0 +1,12 @@
+package quatroprojet;
+
+public class Start
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // Init
+    }
+}
